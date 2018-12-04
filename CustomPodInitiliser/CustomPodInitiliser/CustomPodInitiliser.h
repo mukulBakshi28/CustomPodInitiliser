4b6fe1e8bd41119768c424671e6b390cf4893aac
mukulBakshi28 <40543802+mukulBakshi28@users.noreply.github.com>
2018-12-04 23:49:30 +0530

    Pod First

---
+++ CustomPodInitiliser/CustomPodInitiliser/CustomPodInitiliser.h
@@ -0,0 +1,19 @@
+//
+//  CustomPodInitiliser.h
+//  CustomPodInitiliser
+//
+//  Created by MUKUL on 04/12/18.
+//  Copyright © 2018 CoderWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomPodInitiliser.
+FOUNDATION_EXPORT double CustomPodInitiliserVersionNumber;
+
+//! Project version string for CustomPodInitiliser.
+FOUNDATION_EXPORT const unsigned char CustomPodInitiliserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomPodInitiliser/PublicHeader.h>
+
+
---
+++ CustomPodInitiliser/CustomPodInitiliser/LoaderManager.swift
@@ -0,0 +1,20 @@
+//
+//  LoaderManager.swift
+//  CustomPodInitiliser
+//
+//  Created by MUKUL on 04/12/18.
+//  Copyright © 2018 CoderWork. All rights reserved.
+//
+
+import Foundation
+
+
+public class LoaderManager {
+    
+   public static func testManager() {
+    
+    print("test Manager called")
+    
+    }
+    
+}
